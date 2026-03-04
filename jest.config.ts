330a08d193bc9931f9c9eb3dae72f6a74f19361e
Fabricio Lima <fabriciotrab2005@gmail.com>
2026-03-04 15:39:16 -0300

    chore(tests): setup Jest with ts-jest and testing-library

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
+
+export default config;
